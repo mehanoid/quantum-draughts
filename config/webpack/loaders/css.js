38a9bb398848b21d1609ae73e6fd3f407ed2feab
Oleg Grigoriev <mehanoid.ru@gmail.com>
2017-07-15 17:32:10 +0300

    loaders

--- config/webpack/loaders/css.js
+++ config/webpack/loaders/css.js
@@ -2,14 +2,13 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const { env } = require('../configuration.js')
 
 module.exports = {
-  test: /\.(scss|sass|css)$/i,
+  test: /\.(css)$/i,
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
       { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
       { loader: 'postcss-loader', options: { sourceMap: true } },
       'resolve-url-loader',
-      { loader: 'sass-loader', options: { sourceMap: true } }
     ]
   })
 }
--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -6,8 +6,7 @@ module.exports = {
     loaders: {
       js: 'babel-loader',
       file: 'file-loader',
-      scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
-      sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax'
+      postcss: 'vue-style-loader!css-loader!postcss-loader?sourceMap=true'
     }
   }
 }
