d15d20e6a93dd744e70cc177282dc091d5af8d45
Oleg Grigoriev <mehanoid.ru@gmail.com>
2018-07-31 17:19:06 +0300

    rewrite pie chart

--- app/frontend/gameplay/components/PieChart.vue
+++ app/frontend/gameplay/components/PieChart.vue
@@ -1,75 +1,80 @@
 <template lang="pug">
   svg(:viewBox='viewBox')
-    circle(cx='0', cy='0', r='1', :stroke='background', :stroke-width='strokeWidth / 5', opacity="0.5")
-    path(v-if='!circle', :d='path', :stroke='color', :stroke-width='strokeWidth / 5', :opacity='opacity')
-    circle(v-if='circle', cx='0', cy='0', r='1', :stroke='color', :stroke-width='strokeWidth / 5', :opacity='opacity')
+    circle(
+      v-if='isFull'
+      cx='0'
+      cy='0'
+      :r='radius'
+      :stroke='color'
+      :stroke-width='strokeWidth'
+      fill="transparent"
+    )
+    template(v-else)
+      circle(
+        cx='0'
+        cy='0'
+        :r='radius'
+        :opacity="backgroundOpacity"
+        :stroke='_backgroundColor'
+        :stroke-width='strokeWidth'
+        fill="transparent"
+      )
+      path(
+        :d='arcPathD'
+        :stroke='color'
+        :stroke-width='strokeWidth'
+        fill="transparent"
+      )
 </template>
 
 <script>
-  export default {
-    name: 'pie-chart',
-    props: {
-      percent: {
-        type: Number,
-        default: 1
-      },
-      strokeWidth: {
-        type: Number,
-        default: 1,
-      },
-      color: {
-        type: String,
-        default: '#40a070'
-      },
-      background: {
-        type: String,
-        default: '#75806f'
-      },
-      opacity: {
-        type: Number,
-        default: 1
-      }
+export default {
+  props: {
+    percent: {
+      type: Number,
+      required: true
     },
-    computed: {
-      width: function () {
-        return 2 + this.strokeWidth / 5
-      },
-      viewBox: function () {
-        let c = 1 + this.strokeWidth / 10
-        let w = this.width
-        return `${-c} ${-c} ${w} ${w}`
-      },
-      circle: function () {
-        return this.percent === 100
-      },
-      _ratio: function () {
-        return this.percent / 100
-      },
-      arc: function () {
-        let r = this._ratio  // short hand
-        let deg = 2 * Math.PI * r
-        let x = 1 * Math.sin(deg)
-        let y = -1 * Math.cos(deg)
-        let negative = r < 0
-        let large = 0
-        // 0 ccw, 1 clock-wise
-        let rotation = negative ? 0 : 1
-        if (negative && x > 0) {
-          large = 1
-        } else if (!negative && x < 0) {
-          large = 1
-        }
-        return `A 1 1 0 ${large} ${rotation} ${x} ${y}`
-      },
-      path: function () {
-        return `M 0 -1 ${this.arc}`
-      }
+    width: {
+      type: Number,
+      default: 1,
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    // eslint-disable-next-line vue/require-default-prop
+    backgroundColor: String,
+    backgroundOpacity: {
+      type: Number,
+      default: 0.5
+    }
+  },
+  computed: {
+    radius: () => 1,
+    diameter() {
+      return this.radius * 2
+    },
+    viewBox() {
+      const width = this.diameter + this.strokeWidth
+      const coordinate = -width / 2
+      return `${coordinate} ${coordinate} ${width} ${width}`
+    },
+    strokeWidth() {
+      return this.width * this.radius * 2
+    },
+    _backgroundColor() {
+      return this.backgroundColor || this.color
+    },
+    isFull() {
+      return this.percent === 100
+    },
+    arcPathD() {
+      const degree = 2 * Math.PI * this.percent / 100
+      const x = Math.sin(degree) * this.radius
+      const y = -Math.cos(degree) * this.radius
+      const largeArcFlag = x < 0 ? 1 : 0
+      return `M 0 ${-this.radius} A ${this.radius} ${this.radius} 0 ${largeArcFlag} 1 ${x} ${y}`
     }
   }
+}
 </script>
-
-<style scoped>
-  svg path, svg circle {
-    fill: transparent;
-  }
-</style>
