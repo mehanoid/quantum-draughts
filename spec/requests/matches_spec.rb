007813070310968a815bef46319fbbd4e82efd26
Oleg Grigoriev <mehanoid.ru@gmail.com>
2019-03-28 23:38:03 +0300

    fix spec

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Matches', type: :request do
-  describe 'GET /matches' do
+  describe 'GET /' do
     it 'works! (now write some real specs)' do
-      get game_matches_path
+      get root_path
       expect(response).to have_http_status(200)
     end
   end
