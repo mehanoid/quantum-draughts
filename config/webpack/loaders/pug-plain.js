cc26e96c56161bd8374cfb08b14ca00e3b4a70d2
Oleg Grigoriev <mehanoid.ru@gmail.com>
2019-05-03 19:54:30 +0300

    target blank for external urls

--- config/webpack/loaders/pug-plain.js
+++ config/webpack/loaders/pug-plain.js
@@ -1,4 +1,22 @@
+var md = require('markdown-it')()
+var mila = require('markdown-it-link-attributes')
+
+md.use(mila, {
+  pattern: /^https?:/,
+  attrs: {
+    target: '_blank',
+    rel: 'noopener'
+  }
+})
+
 module.exports = {
   test: /\.pug$/,
   loader: 'pug-plain-loader',
+  options: {
+    filters: {
+      'markdown': function (str, options) {
+        return md.render(str);
+      }
+    },
+  }
 }
