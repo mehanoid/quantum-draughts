1d6c712d4b2c74fbadffcc4839f5a7e734b96801
Oleg Grigoriev <mehanoid.ru@gmail.com>
2023-10-28 18:14:01 +0300

    update ruby to 3.0

--- app/lib/gameplay/russian_ruleset.rb
+++ app/lib/gameplay/russian_ruleset.rb
@@ -3,7 +3,7 @@
 module Gameplay
   module RussianRuleset
     class << self
-      def build_move_step(board, move_cells, *args)
+      def build_move_step(board, move_cells, *args, **kwargs)
         from_cell = board.cell_at(move_cells.first)
         klass     =
           if from_cell.draught&.king?
@@ -11,7 +11,7 @@ module Gameplay
           else
             self::ManMoveStep
           end
-        klass.new(board, move_cells, *args)
+        klass.new(board, move_cells, *args, **kwargs)
       end
 
       def max_step_length(cell)
