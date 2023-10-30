8d7c6aae9646588e5d02e9a5a854f2ab48510d86
Oleg Grigoriev <mehanoid.ru@gmail.com>
2023-10-30 18:58:13 +0300

    add simplecov

--- Gemfile
+++ Gemfile
@@ -90,6 +90,7 @@ group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15', '< 4.0'
   gem 'selenium-webdriver'
+  gem 'simplecov', require: false
   # Easy installation and use of chromedriver to run system tests with Chrome
   # gem 'webdrivers'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,14 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'simplecov'
+SimpleCov.start('rails') do
+  enable_coverage :branch
+  add_group 'Interactors', 'app/interactors'
+  add_group 'Serializers', 'app/serializers'
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
